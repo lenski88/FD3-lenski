efb76747cacd501b37860b25714e310b5ff7faa5
lenski <lenski88@icloud.com>
2021-04-03 15:39:41 +0300

    ...

--- ishop/CatalogIShop.js
+++ ishop/CatalogIShop.js
@@ -1,10 +1,20 @@
-let CatalogIShop = React.createClass ({
+let CatalogIShop = React.createClass({
+  displayName: "CatalogIShop",
 
-    displayName : "CatalogIShop",
+  render: function () {
+    let catalogCode = this.props.catalog.map((i) =>
+        React.DOM.tr({ key: i.code, className: "CatalogTr" },
+        React.DOM.td({ className: "CatalogTd" }, i.name),
+        React.DOM.td({ className: "CatalogTd" }, i.price),
+        React.DOM.td({ className: "CatalogTd" }, i.urlImage),
+        React.DOM.td({ className: "CatalogTd" }, i.balance)
+      )
+    );
 
-    render: function () {
-
-        
-    }
-
+    return React.DOM.div(
+      { className: "CatalogIShop" },
+      React.DOM.div({ className: "Nameshop" }, this.props.nameShop),
+      React.DOM.table({ className: "CatalogTable" }, catalogCode),
+    );
+  },
 });
