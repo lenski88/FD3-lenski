89c1d8f4dd187b3f8a8ab104659ef4d6dd7ea68c
lenski <lenski88@icloud.com>
2021-04-04 10:49:53 +0300

    ...

--- ishop/CatalogIShop.js
+++ ishop/CatalogIShop.js
@@ -2,11 +2,21 @@ let CatalogIShop = React.createClass({
   displayName: "CatalogIShop",
 
   render: function () {
+
+    let headerTable = this.props.catalogHd.map((i) =>
+        React.DOM.tr({key:i.code, className:"CatalogTr"},
+        React.DOM.th({className:"CatalogTd"}, i.nameHeader,),
+        React.DOM.th({className:"CatalogTd"}, i.priceHeader),
+        React.DOM.th({className:"CatalogTd"}, i.urlImageHeader),
+        React.DOM.th({className:"CatalogTd"}, i.balanceHeader),
+        )
+    );
+    
     let catalogCode = this.props.catalog.map((i) =>
         React.DOM.tr({ key: i.code, className: "CatalogTr" },
         React.DOM.td({ className: "CatalogTd" }, i.name),
         React.DOM.td({ className: "CatalogTd" }, i.price),
-        React.DOM.td({ className: "CatalogTd" }, i.urlImage),
+        React.DOM.td({ className: "CatalogTd" }, React.DOM.img({className: "CatalogImage", src:i.urlImage})),
         React.DOM.td({ className: "CatalogTd" }, i.balance)
       )
     );
@@ -14,7 +24,7 @@ let CatalogIShop = React.createClass({
     return React.DOM.div(
       { className: "CatalogIShop" },
       React.DOM.div({ className: "Nameshop" }, this.props.nameShop),
-      React.DOM.table({ className: "CatalogTable"}, catalogCode),
+      React.DOM.table({ className: "CatalogTable"}, React.DOM.tbody({className:"CatalogTBody"}, headerTable, catalogCode)),
     );
   },
 });
