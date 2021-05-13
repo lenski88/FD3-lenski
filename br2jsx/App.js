5975ca4a1b8823f144a9709a2314d1b257cbf3b9
lenski <lenski88@icloud.com>
2021-05-13 23:42:32 +0300

    ...

--- br2jsx/App.js
+++ br2jsx/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+import BR2JSX from './components/BR2JSX'
+
 let text="первый<br>второй<br/>третий<br />последний";
 
 ReactDOM.render (
--- br2jsx/components/BR2JSX.js
+++ br2jsx/components/BR2JSX.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 class BR2JSX extends React.Component {
-    static PropTypes = {
-        text: PropTypes.string.isRequired
-    }
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+  };
 
-    render () {
-
-        
-    }
+  render() {
+    let words = this.props.text.split(/<br\s?\/?>/i);
+    let parts = [];
+    words.forEach((w, i) => {
+      if (i) {
+        parts.push(<br />);
+      }
+      parts.push(w);
+    });
+    return (
+      <div
+        style={{
+          width: "400px",
+          backgroundColor: "black",
+          color: "white",
+          textAlign: "center",
+        }}
+      >
+        {parts}
+      </div>
+    );
+  }
 }
 
 export default BR2JSX;
